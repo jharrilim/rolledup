3e465dd6be6635a66aa0515aa10a21d994f54936
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2020-09-02 22:47:33 -0400

    Wheelie

--- jsxdemo.jsx
+++ jsxdemo.jsx
@@ -1,5 +1,16 @@
+const { useEffect, useCallback } = React;
 
 const DemoApp = () => {
+
+  const wheelies = useCallback(() => {
+    console.debug('360 WhEeL');
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('wheel', wheelies);
+    return () => window.removeEventListener('wheel', wheelies);
+  });
+
   return (
     <div>
       <div>Hello Moto</div>
