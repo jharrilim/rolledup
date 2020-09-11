3487603d2230a1fa2039112a213724c14c3b2ac2
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2020-09-11 15:32:10 -0400

    Add example with localStorage

--- examples/proj/foo.js
+++ examples/proj/foo.js
@@ -1,12 +1,17 @@
 import Title from './title';
 
 class Foo extends React.Component {
+  constructor(props) {
+    super(props);
+    localStorage.setItem('count', 'Uno Dos Tres Cuatro');
+  }
+
   render() {
     return (
       <div>
         <Title />
         <h2>Dale</h2>
-        <h3>Uno Dos Tres</h3>
+        <h3>{localStorage.getItem('count') || '305'}</h3>
       </div>
     );
   }
